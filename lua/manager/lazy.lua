429a63c832a36ced1529cd068d57870065030127
Xeckt <jordansavell1198@gmail.com>
2025-06-11 18:07:26 +0100

    add theme for tests

--- lua/manager/lazy.lua
+++ lua/manager/lazy.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "darcula" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
@@ -38,3 +38,5 @@ require("lazy").setup({
 require("user.diagnostic")
 require("user.keymaps")
 require("user.options")
+
+vim.cmd.colorscheme("tokyonight-night")
