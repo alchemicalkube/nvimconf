e09b9e3e233c8c0a3a6222b85ce2b3671645f8a7
Xeckt <jordansavell1198@gmail.com>
2025-06-11 17:22:39 +0100

    update lsp to include runtime for vim api

--- lua/plugins/code/lsp.lua
+++ lua/plugins/code/lsp.lua
@@ -49,7 +49,20 @@ return {
             -------------
             --- Lua
             -------------
-            vim.lsp.enable('lua_ls')
+            vim.lsp.enable("lua_ls")
+            vim.lsp.config("lua_ls", {
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = "LuaJIT"
+                        },
+                        diagnostics = { globals = { "vim" } },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                    },
+                },
+            })
             -------------
             --- Lua
             -------------
