d507c3d83a3c2b7de560ece0463805c01cb46cfd
Xeckt <jordansavell1198@gmail.com>
2025-06-12 10:01:46 +0100

    add more keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -9,7 +9,20 @@ vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 -------------
 --- Editor
 -------------
-
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
 -------------
 --- Editor
+-------------
+
+-------------
+--- Telescope
+-------------
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+-------------
+--- Telescope
 -------------
